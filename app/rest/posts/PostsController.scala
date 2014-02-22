329f44a789df02583a6b43721d3deceee6b4cf85
ychartois <yannickchartois@wanadoo.fr>
2014-02-22 15:51:32 +0100

    getPosts implementation

--- app/rest/posts/PostsController.scala
+++ app/rest/posts/PostsController.scala
@@ -1,12 +1,46 @@
 package rest.posts
 
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
 import play.api._
 import play.api.mvc._
+import play.modules.reactivemongo.MongoController
+import scala.concurrent.Future
+import play.api.libs.json._
+import play.modules.reactivemongo.json.collection.JSONCollection
 
-object PostsController extends Controller {
+object PostsController extends Controller with MongoController {
 
-  def getPosts: Action[AnyContent] = Action {
-    Ok("works")
+  /**
+   * Get the posts collection
+   *
+   * @return the posts collection
+   */
+  def posts: JSONCollection = db.collection[JSONCollection]("posts")
+
+
+  /**
+   * Get all the blog posts
+   *
+   * @return a json array with all the posts information
+   */
+  def getPosts: Action[AnyContent] = Action.async {
+
+    // We want all the posts
+    val cursor = posts.find( Json.obj() ).cursor[JsObject]
+
+    // gather all the JsObjects in a list
+    val postsList: Future[List[JsObject]] = cursor.collect[List]()
+
+    // transform the list into a JsArray
+    val postsArray: Future[JsArray] = postsList.map { posts =>
+      Json.arr(posts)
+    }
+
+    postsArray.map {  posts =>
+      Ok( posts.apply(0) )
+    }
   }
 
 }
